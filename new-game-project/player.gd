d8bd32e53e4a6dbdc487ea4a77228d9584ad2d33
mir927 <Ahwolridge@s.sfusd.edu>
2026-03-24 14:16:53 -0700

    finished progress check 3

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
+signal OnUpdateHealth (health : int)
+signal OnUpdateScore (score : int)
+
 @export var move_speed : float = 100
 @export var acceleration : float = 50
 @export var braking : float = 20
 @export var gravity : float = 500 
 @export var jump_force : float = 200
+
+@export var health : int = 3
 
 var move_input : float
 
@@ -41,3 +46,22 @@ func _manage_animation():
 		anim.play("move")
 	else:
 		anim.play("idle")
+
+func take_damage (amount : int):
+	health -= amount
+	OnUpdateHealth.emit(health)
+	if health <= 0:
+		call_deferred("game_over")
+	_damage_flash()
+
+func game_over ():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	
+func increase_score (amount : int):
+	PlayerStats.score += amount
+	OnUpdateScore.emit(PlayerStats.score)
+
+func _damage_flash ():
+	sprite.modulate = Color.RED
+	await get_tree().create_timer(0.05).timeout
+	sprite.modulate = Color.WHITE
